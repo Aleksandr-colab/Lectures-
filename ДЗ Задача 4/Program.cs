a072befcdceba93168f8b499842e18161086395d
“[Aleksandr] <[fraoo@icloud.coml]>
2023-02-01 23:28:34 +0300

    Решение задач к семенару №2

--- ДЗ Задача 4/Program.cs
+++ ДЗ Задача 4/Program.cs
@@ -1,12 +1,28 @@
 ﻿// Задача 4: Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
 Console.Clear();
 Console.WriteLine("Введите первое число ");
-int a = int.Parse(Console.ReadLine()!);
+int num_1 = int.Parse(Console.ReadLine()!);
 Console.WriteLine("Введите второе число ");
-int b = int.Parse(Console.ReadLine()!);
+int num_2 = int.Parse(Console.ReadLine()!);
 Console.WriteLine("Введите третье число ");
-int c = int.Parse(Console.ReadLine()!);
-if (a > b && a > c) Console.WriteLine($"Максимальное первое число={a}");
-else if (b > a && b > c) Console.WriteLine($"Максимальное второе число={b}");
-else if (c > a && c > b) Console.WriteLine($"Максимальное третье число={c}");
-else if (a == b && a == c && b == c) Console.WriteLine("Введенные максимальные числа равны");
+int num_3 = int.Parse(Console.ReadLine()!);
+if (num_1 > num_2) 
+{
+   if (num_1 > num_3)
+   {
+        Console.WriteLine($"максимальное число: {num_1}");
+   }
+    else
+    {
+        Console.WriteLine($"максимальное число: {num_3}");
+    }
+}
+else if (num_2 > num_3)
+{
+    Console.WriteLine($"максимальное число: {num_2}");
+}
+else
+{
+    Console.WriteLine($"максимальное число: {num_3}");
+}
+
