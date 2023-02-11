fe6e6a1bd82547eebbf6c9b886430ff265e8a3ac
“[Aleksandr] <[fraoo@icloud.coml]>
2023-02-11 11:46:32 +0300

    Решение задач на Семинаре 5

---
+++ Задача 35 Семинар 5/Program.cs
@@ -0,0 +1,36 @@
+﻿// Задача 35: Задайте одномерный массив из 123 случайных чисел.
+// Найдите количество элементов массива, значения которых лежат вотрезке [10,99].
+//Пример для массива из 5, а не 123 элементов. В своём решении сделайте для 123
+//[5, 18, 123, 6, 2] -> 1
+//[1, 2, 3, 6, 2] -> 0
+//[10, 11, 12, 13, 14] -> 5
+Console.Clear();
+int[] GetArray(int size, int minValue, int maxValue)
+{
+    int[] result = new int[size];
+
+    for (int i = 0; i < size; i++)
+    {
+        result[i] = new Random().Next(minValue, maxValue + 1);
+    }
+    return result;
+}
+
+
+int CountNumbers(int[] array)
+{
+    int count = 0;
+    foreach(int element in array)
+    {
+        if (element >= 10 && element <= 99 )
+        {
+            count++;
+        }
+    }
+    return count;
+}
+
+int[] array = GetArray(123, 0, 150);
+Console.WriteLine(String.Join(", ", array));
+int count = CountNumbers(array);
+Console.WriteLine(count);
---
+++ Задача 37 Семинар  5/Program.cs
@@ -0,0 +1,40 @@
+﻿// Задача 37: Найдите произведение пар чисел в одномерном массиве.
+// Парой считаем первый и последний элемент, второй и предпоследний и т.д.
+// Результат запишите в новом массиве. [1 2 3 4 5] -> 5 8 3
+// [6 7 3 6] -> 36 21
+
+Console.Clear();
+int[] GetArray(int size, int minValue, int maxValue)
+{
+    int[] result = new int[size];
+
+    for (int i = 0; i < size; i++)
+    {
+        result[i] = new Random().Next(minValue, maxValue + 1);
+    }
+    return result;
+}
+
+int[] MultiArray(int[] array)
+{
+    int size = array.Length / 2;
+    if (array.Length % 2 == 1)
+    {
+        size++;
+    }
+    int[] result = new int[size];
+    for (int i = 0; i < array.Length / 2; i++)
+    {
+        result[i] = array[i] * array[array.Length -1 - i];
+    }
+    if (array.Length % 2 == 1)
+    {
+        result[size -1] = array[array.Length / 2];
+    }
+    return result;
+}
+int[] array = GetArray(4, 0, 10);
+Console.WriteLine(String.Join(", ", array));
+
+int[] multiArray = MultiArray(array);
+Console.WriteLine(String.Join(", ", multiArray));
