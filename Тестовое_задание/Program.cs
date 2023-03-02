03efa91c97bd8d22edb48db5a367b8bb0cdfd5af
“[Aleksandr] <[fraoo@icloud.coml]>
2023-03-02 23:12:08 +0300

    Решение ДЗ к семинару 9

--- Тестовое_задание/Program.cs
+++ Тестовое_задание/Program.cs
@@ -1,4 +1,4 @@
-﻿// // Задача 2: Напишите программу, которая на вход принимает два числа и выдает, 
+﻿// // Задача 2: Напишите программу, которая на вход принимает два числа и выдает,
 // // какое число большее, а какое меньшее.
 
 // Console.Clear();
@@ -9,17 +9,17 @@
 // int number2 = int.Parse (Console.ReadLine()!);
 
 //  if (number1 > number2)
- 
+
 // {
 //     Console.WriteLine($"Первое число {number1} больше второго числа {number2}");
-// } 
+// }
 //  if (number2 > number1)
-  
+
 // {
 //     Console.WriteLine($"Второе число {number2} больше первого числа {number1}");
 // }
 
-// Напишите программу, которая принимает на вход три числа 
+// Напишите программу, которая принимает на вход три числа
 // и выдает максимальное из этих чисел.
 
 // Console.Clear();
@@ -38,11 +38,11 @@
 //         Console.WriteLine($"Максимальное число {number1} ");
 //     }
 //      else
-//     {   
+//     {
 //         Console.WriteLine($"Максимальное число {number3} ");
 //     }
 // }
-//     else if (number2 > number3) 
+//     else if (number2 > number3)
 //     {
 //         Console.WriteLine($"Максимальное число {number2} ");
 //     }
@@ -53,7 +53,7 @@
 
 
 
-// Напишите программу, которая на вход принимает число и выдает, 
+// Напишите программу, которая на вход принимает число и выдает,
 // является ли число четным (делится ли оно на два без остатка).
 // Console.Clear();
 
@@ -82,3 +82,63 @@
 //     Console.WriteLine($"четные числа до {numberB}");
 //     numberB +=2 ;
 // }
+
+//Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+
+
+// Console.Clear();
+
+// Console.WriteLine("Введите трехзначное число: ");
+// int num = int.Parse (Console.ReadLine()!);
+// int num1 = num / 10;
+// int num2 = num1 % 10;
+// Console.WriteLine($"{num2}");
+
+// Console.Clear();
+
+// Console.WriteLine("Введите  число: ");
+// int num = int.Parse (Console.ReadLine()!);
+
+// if (num < 100)
+// {
+//     Console.WriteLine("В этом числе третья цифра отсутствует: ");
+// }
+// while ( num >= 100)
+// {
+//     if (num < 1000)
+//     {
+//     int Number = num % 10;
+//     Console.WriteLine("Третья цифра в числе: " + Number);
+//     }
+//    num = num / 10;
+// }
+Console.Clear();
+
+Console.WriteLine("Введите  число: ");
+int num = int.Parse(Console.ReadLine()!);
+string[] The_days_of_the_week =
+{
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+    "Sunday"
+};
+int Day = num - 1;
+if (Day < The_days_of_the_week.Length)
+{
+    if (num <= 5)
+    {
+        Console.WriteLine("Рабочий день: ");
+    }
+    else
+    {
+        Console.WriteLine("это выходной день: ");
+    }
+}
+else
+{
+    Console.WriteLine("эТакого дня не существует: ");
+}
